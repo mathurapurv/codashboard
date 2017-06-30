9ca9553919a3381e34d53709c4256cbc12425eb6
Apurv <mathur.apurv@gmail.com>
2017-07-01 01:03:11 +0530

    adding files

---
+++ src/main/java/com/apurv/codashboard/CodashboardApplication.java
@@ -0,0 +1,12 @@
+package com.apurv.codashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodashboardApplication.class, args);
+	}
+}
---
+++ src/test/java/com/apurv/codashboard/CodashboardApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apurv.codashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CodashboardApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
